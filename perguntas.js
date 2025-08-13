27a39f0bf760adf73d93286fbd82a18e882fff3c
sibenlindo <joao.siben@escola.pr.gov.br>
2025-08-13 07:36:32 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -9,3 +9,8 @@ criaCartao(
     'Suas máquinas são de boa qualidade?',
     'As máquinas da Wahl são conhecidas pela durabilidade, precisão e desempenho consistente, sendo muito apreciadas por profissionais. Destacam-se pela ergonomia, lâminas de alta qualidade e potência do motor, que proporcionam cortes rápidos e eficientes. Além disso, oferecem variedade de acessórios e ajustes de corte. Apesar de serem um pouco mais barulhentas, são uma excelente opção para quem busca qualidade e confiabilidade.'
 )
+
+criaCartao(
+    'Marca Wahl',
+    'Qual a origem da Wahl?',
+    'A Wahl foi fundada nos Estados Unidos. Mais precisamente, a Wahl Clipper Corporation foi estabelecida em 1919 em Sterling, Illinois, por Leo J. Wahl. Ele é reconhecido como o inventor da primeira máquina de cortar cabelo elétrica prática com motor interno. '
